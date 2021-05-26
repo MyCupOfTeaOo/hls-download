cf5d0e6976f67ea45ea62dc965fe2f46135512d3
zou wendi <542095059@qq.com>
2021-05-26 11:14:50 +0800

    feat: 支持多种下载器,并且增加示例

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from download import Download
+from download import Download, NunuyyDownload
 import asyncio
 import sys
 import logging
@@ -9,12 +9,16 @@ import argparse
 import subprocess
 import os
 import glob
-from download import ts_pattern, name_filter_pattern, key_pattern, file_suffix
 
 _suffix = re.compile('\n$')
 
 LOG_FORMAT = "%(asctime)s %(name)s-%(levelname)s(%(filename)s:%(funcName)s): %(message)s"
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+DOWNLOAD_MAP = {
+    "download": Download,
+    "nunuyydownload": NunuyyDownload
+}
 
 
 class QueueHandler(logging.Handler):
@@ -43,6 +47,8 @@ if __name__ == "__main__":
                         help="代理地址,默认http://127.0.0.1:1080", default="http://127.0.0.1:1080")
     parser.add_argument("--no-proxy", action="store_true",
                         help="关闭代理")
+    parser.add_argument("--download", default="download",
+                        help="下载器设置")
 
     args = parser.parse_args()
 
@@ -52,27 +58,15 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG, handlers=(
         cust_handler,))
     init()
-    down = Download(args.name,
-                    args.url, proxy=args.proxy if not args.no_proxy else None, process_num=args.process)
+    TargetDownLoad = DOWNLOAD_MAP[args.download.lower()]
+    down = TargetDownLoad(args.name,
+                          args.url, proxy=args.proxy if not args.no_proxy else None, process_num=args.process)
     asyncio.run(down.go())
     # 需要处理下带querystring的文件名
-    m3u8_name = name_filter_pattern.sub("", args.url.split("/")
-                                        [-1])
+    m3u8_name = TargetDownLoad.name_filter_pattern.sub("", args.url.split("/")
+                                                       [-1])
     # 需要处理下m3u8文件防止文件名不一致
-    with open(f'{down._path}/{m3u8_name}', encoding="utf-8", mode="r+") as f:
-        m3u8_file = f.read()
-        key_res = key_pattern.search(m3u8_file)
-        if key_res:
-            key_name = key_res.group()
-            m3u8_file = m3u8_file.replace(
-                key_name, name_filter_pattern.sub("", file_suffix.sub(".ts", key_name.split("/")[-1])))
-        ts_list = ts_pattern.findall(m3u8_file)
-        for ts_link in ts_list:
-            m3u8_file = m3u8_file.replace(
-                ts_link, name_filter_pattern.sub("", ts_link.split("/")[-1]))
-        f.truncate(0)
-        f.seek(0)
-        f.write(m3u8_file)
+    TargetDownLoad.replace_m3u8(f'{down._path}/{m3u8_name}')
     cmd = ["ffmpeg", "-i", m3u8_name, "-movflags", "faststart", "-c", "copy",
            f"{args.name}.mp4"]
     logging.info(f"执行命令: {' '.join(cmd)}")
